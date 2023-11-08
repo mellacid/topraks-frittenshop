b521801302fd154f769b24c7cda48d5448357f40
dci-student <lailabeqiri@libero.it>
2023-11-08 13:43:39 +0100

    Fritten bug img fixed

--- data/fritten.js
+++ data/fritten.js
@@ -1,6 +1,6 @@
 export const fritten = [
-  { name: "Menü 1", image: ".././img/menu-11.jpg", price: 8.7 },
-  { name: "Menü 2", image: ".././img/menu-12.jpg", price: 9.4 },
-  { name: "Menü 3", image: ".././img/menu-13.jpg", price: 9.4 },
-  { name: "Menü 4", image: ".././img/menu-14.jpg", price: 9.7 },
+  { name: "Menü 1", image: "./img/menu-11.jpg", price: 8.7 },
+  { name: "Menü 2", image: "./img/menu-12.jpg", price: 9.4 },
+  { name: "Menü 3", image: "./img/menu-13.jpg", price: 9.4 },
+  { name: "Menü 4", image: "./img/menu-14.jpg", price: 9.7 },
 ];
