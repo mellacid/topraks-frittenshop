b6921bf034ec076fad2840805bd90c882b142853
Jan <j.c.scherch@gmail.com>
2023-11-08 11:23:00 +0100

    Fritten Array added

--- data/fritten.js
+++ data/fritten.js
@@ -1,1 +1,6 @@
-export const fritten = ["test", "test2"];
+export const fritten = [
+  { name: "Menü 1", image: ".././img/menu-11.jpg", price: 8.7 },
+  { name: "Menü 2", image: ".././img/menu-12.jpg", price: 8.7 },
+  { name: "Menü 3", image: ".././img/menu-13.jpg", price: 8.7 },
+  { name: "Menü 4", image: ".././img/menu-14.jpg", price: 8.7 },
+];
